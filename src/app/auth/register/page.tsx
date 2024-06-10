dfa77b7526be152e8dc462976aa209b5623f0f67
Osman Ali <osman_ali73@yahoo.com>
2024-06-11 04:12:35 +0800

    added sign in form in login page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,4 +1,38 @@
+import Link from "next/link";
 import React from "react";
+
+const fields = [
+  {
+    id: 1,
+    type: "text",
+    name: "name",
+    label: "Name",
+  },
+  {
+    id: 2,
+    type: "email",
+    name: "email",
+    label: "Email address",
+  },
+  {
+    id: 3,
+    type: "password",
+    name: "password",
+    label: "Password",
+  },
+  {
+    id: 4,
+    type: "password",
+    name: "password_confirmation",
+    label: "Confirm Password",
+  },
+  {
+    id: 5,
+    type: "checkbox",
+    name: "terms",
+    label: "",
+  },
+];
 
 const Register = () => {
   return (
@@ -11,12 +45,12 @@ const Register = () => {
             </h1>
             <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
               Already have an account?
-              <a
+              <Link
                 className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
-                href="../examples/html/signin.html"
+                href="/auth/login"
               >
                 Sign in here
-              </a>
+              </Link>
             </p>
           </div>
 
@@ -58,142 +92,52 @@ const Register = () => {
 
             <form>
               <div className="grid gap-y-4">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm mb-2 dark:text-white"
-                  >
-                    Email address
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                      required
-                      aria-describedby="email-error"
-                    />
-                    <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                      <svg
-                        className="size-5 text-red-500"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                        aria-hidden="true"
-                      >
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                      </svg>
-                    </div>
-                  </div>
-                  <p
-                    className="hidden text-xs text-red-600 mt-2"
-                    id="email-error"
-                  >
-                    Please include a valid email address so we can get back to
-                    you
-                  </p>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm mb-2 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                      required
-                      aria-describedby="password-error"
-                    />
-                    <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                      <svg
-                        className="size-5 text-red-500"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                        aria-hidden="true"
-                      >
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                      </svg>
-                    </div>
-                  </div>
-                  <p
-                    className="hidden text-xs text-red-600 mt-2"
-                    id="password-error"
-                  >
-                    8+ characters required
-                  </p>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="confirm-password"
-                    className="block text-sm mb-2 dark:text-white"
-                  >
-                    Confirm Password
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="password"
-                      id="confirm-password"
-                      name="confirm-password"
-                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                      required
-                      aria-describedby="confirm-password-error"
-                    />
-                    <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                      <svg
-                        className="size-5 text-red-500"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                        aria-hidden="true"
-                      >
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                      </svg>
-                    </div>
-                  </div>
-                  <p
-                    className="hidden text-xs text-red-600 mt-2"
-                    id="confirm-password-error"
-                  >
-                    Password does not match the password
-                  </p>
-                </div>
-
-                <div className="flex items-center">
-                  <div className="flex">
-                    <input
-                      id="remember-me"
-                      name="remember-me"
-                      type="checkbox"
-                      className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                    />
-                  </div>
-                  <div className="ms-3">
-                    <label
-                      htmlFor="remember-me"
-                      className="text-sm dark:text-white"
-                    >
-                      I accept the{" "}
-                      <a
-                        className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
-                        href="#"
-                      >
-                        Terms and Conditions
-                      </a>
-                    </label>
-                  </div>
-                </div>
+                {fields.length > 0 &&
+                  fields.map((field) => {
+                    return field.name === "terms" ? (
+                      <div className="flex items-center">
+                        <div className="flex">
+                          <input
+                            id={field.id.toString()}
+                            name={field.name}
+                            type={field.type}
+                            className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                          />
+                        </div>
+                        <div className="ms-3">
+                          <label
+                            htmlFor={field.id.toString()}
+                            className="text-sm dark:text-white"
+                          >
+                            I accept the{" "}
+                            <Link
+                              className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
+                              href="#"
+                            >
+                              Terms and Conditions
+                            </Link>
+                          </label>
+                        </div>
+                      </div>
+                    ) : (
+                      <div key={field.id}>
+                        <label
+                          htmlFor={field.id.toString()}
+                          className="block text-sm mb-2 dark:text-white"
+                        >
+                          {field.label}
+                        </label>
+                        <div className="relative">
+                          <input
+                            type={field.type}
+                            id={field.id.toString()}
+                            name={field.name}
+                            className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
 
                 <button
                   type="submit"
